d0f4e75bea6456c39a4bf8c3813fbdc6094d8b0c
inzaf-ebrahim <insafibrahim102@gmail.com>
2024-06-27 17:00:54 +0530

    first episode

---
+++ app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("div", {},[ React.createElement("h1",{},'hello'), React.createElement("h2",{},'hello i am inzaf ebrahim')]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
